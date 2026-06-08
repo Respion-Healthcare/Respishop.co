040ad6676c573449486d47f9fa37f7638ce89116
shubhashree500 <shreeshubha98@gmail.com>
2026-06-08 14:22:34 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -3,35 +3,39 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
 
-  async redirects() {
-    return [
-      // REMOVE WWW
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.respishop.in",
-          },
-        ],
-        destination: "https://respishop.in/:path*",
-        permanent: true,
-      },
+ async redirects() {
+  return [
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "host",
+          value: "www.respishop.in",
+        },
+      ],
+      destination: "https://respishop.in/:path*",
+      permanent: true,
+    },
 
-      // OLD AUTHOR/TAG URLS
-      {
-        source: "/author/:slug*",
-        destination: "/blog",
-        permanent: true,
-      },
+    {
+      source: "/author/:slug*",
+      destination: "/blog",
+      permanent: true,
+    },
 
-      {
-        source: "/tag/:slug*",
-        destination: "/blog",
-        permanent: true,
-      },
-    ];
-  },
+    {
+      source: "/tag/:slug*",
+      destination: "/blog",
+      permanent: true,
+    },
+
+    {
+      source: "/category/bipap-machine",
+      destination: "/products/bipap-machine",
+      permanent: true,
+    },
+  ];
+},
 };
 
 export default nextConfig;
